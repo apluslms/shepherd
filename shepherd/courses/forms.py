5aec5bb75f4099dea8f63569905be9875c4dc64f
Ruiyang Ding <dry950526@gmail.com>
2019-04-17 15:55:06 +0300

    Course management

--- shepherd/courses/forms.py
+++ shepherd/courses/forms.py
@@ -1,8 +1,14 @@
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, validators, TextAreaField
 
 
 class CourseForm(Form):
     key = StringField('Course Key', [validators.Length(max=50)])
     name = StringField('Course Name', [validators.Length(max=50)])
     git_origin = StringField('Git Origin', [validators.Length(max=255)])
-    git_branch = StringField('Git Branch', [validators.Length(max=50)])
+
+
+class InstanceForm(Form):
+    key = StringField('Instance Key', [validators.Length(max=50)])
+    branches = TextAreaField('Git Branches (Separate by space)', [validators.Length(max=255)])
+
+
--- shepherd/courses/models.py
+++ shepherd/courses/models.py
@@ -7,18 +7,15 @@ from flask import current_app
 class CourseRepository(db.Model):
     key = db.Column(db.String(50), unique=True, primary_key=True)
     git_origin = db.Column(db.String(255), default='')
-    git_branch = db.Column(db.String(50), default='')
     name = db.Column(db.String(50), default='')
     owner = db.Column(db.String(current_app.config['USER_NAME_LENGTH']))
-    updates = db.relationship('CourseRepositoryUpdate', backref='course_repository', lazy=True)
+    instance = db.relationship('CourseInstance', backref='course_repository', lazy=True)
 
 
-class CourseRepositoryUpdate(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    course_id = db.Column(db.String(255), db.ForeignKey('course_repository.key'), nullable=False)
-    request_ip = db.Column(db.String(50), default='')
-    request_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    updated_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    updated = db.Column(db.Boolean, default=False)
-    log = db.Column(db.Text, default="")
+class CourseInstance(db.Model):
+    key = db.Column(db.String(50), primary_key=True)
+    course_key = db.Column(db.String(255), db.ForeignKey('course_repository.key'), nullable=False)
+    branches = db.Column(db.String, default='master')
 
+    def get_branches(self):
+        return [each for each in self.branches.split(' ')]
