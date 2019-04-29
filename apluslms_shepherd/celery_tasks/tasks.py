b576c55a3d166fdbf78dad195800fe2f960f7952
Ruiyang Ding <dry950526@gmail.com>
2019-04-29 15:55:41 +0300

    App 'publish' status to the task

--- apluslms_shepherd/celery_tasks/tasks.py
+++ apluslms_shepherd/celery_tasks/tasks.py
@@ -2,7 +2,7 @@ import subprocess
 
 from celery.utils.log import get_task_logger
 from celery.signals import before_task_publish, task_prerun, after_task_publish, task_postrun, task_success, \
-    task_failure
+    task_failure, task_received
 from datetime import datetime
 
 from apluslms_shepherd.build.models import Build, BuildLog, States, Action
@@ -34,42 +34,6 @@ def build_repo(self):
     pass
 
 
-# @before_task_publish.connect(sender='apluslms_shepherd.celery_tasks.tasks.pull_repo')
-# def clone_task_before_publish(sender=None, headers=None, body=None, **kwargs):
-#     # information about task are located in headers for task messages
-#     # using the task protocol version 2.
-#     info = headers if 'task' in headers else body
-#     print('before_task_publish for task id {info[id]}'.format(
-#         info=info,
-#     ))
-#     res = eval(headers['argsrepr'])
-#     course_key = res[-2]
-#     instance_key = res[-1]
-#     print('course_key:{}, instance_key:{}'.format(course_key, instance_key))
-#     # Change build status
-#     now = datetime.utcnow()
-#     new_build = Build(
-#         course_key=course_key,
-#         instance_key=instance_key,
-#         start_time=now,
-#         status=States.PUBLISH,
-#         action=Action.CLONE
-#     )
-#     print('build')
-#     new_log_entry = BuildLog(
-#         task_id=info["id"],
-#         course_key=course_key,
-#         instance_key=instance_key,
-#         start_time=now,
-#         status=States.PUBLISH,
-#         action=Action.CLONE
-#     )
-#     print('build_log')
-#     db.session.add(new_build)
-#     db.session.add(new_log_entry)
-#     db.session.commit()
-
-
 @task_prerun.connect
 def clone_task_prerun(task_id=None, sender=None, *args, **kwargs):
     # information about task are located in headers for task messages
@@ -83,6 +47,11 @@ def clone_task_prerun(task_id=None, sender=None, *args, **kwargs):
     ))
     logger.info('course_key:{}, instance_key:{}'.format(course_key, instance_key))
     with celery.app.app_context():
+        old_log_entry = BuildLog.query.filter_by(
+            task_id=task_id+'-PUBLISH',
+            action=Action.CLONE if sender.__name__ is 'pull_repo' else Action.BUILD
+        ).first()
+        old_log_entry.end_time = now
         new_log_entry = BuildLog(
             task_id=task_id+'-RUNNING',
             course_key=course_key,
@@ -100,6 +69,42 @@ def clone_task_prerun(task_id=None, sender=None, *args, **kwargs):
             build.status = States.RUNNING
         db.session.add(new_log_entry)
         db.session.commit()
+
+
+@before_task_publish.connect(sender='apluslms_shepherd.celery_tasks.tasks.pull_repo')
+def clone_task_before_publish(sender=None, headers=None, body=None, **kwargs):
+    # information about task are located in headers for task messages
+    # using the task protocol version 2.
+    info = headers if 'task' in headers else body
+    print('before_task_publish for task id {info[id]}'.format(
+        info=info,
+    ))
+    res = eval(headers['argsrepr'])
+    course_key = res[-2]
+    instance_key = res[-1]
+    print('course_key:{}, instance_key:{}'.format(course_key, instance_key))
+    # Change build status
+    now = datetime.utcnow()
+    build = Build.query.filter_by(course_key=course_key, instance_key=instance_key).first()
+    if build is None:
+        build = Build(course_key=course_key, instance_key=instance_key, start_time=now, status=States.PUBLISH,
+                      action=Action.PUBLISH if sender.__name__ is 'pull_repo' else Action.BUILD.CLONE)
+        db.session.add(build)
+    else:
+        build.status = States.RUNNING
+    print('build')
+    new_log_entry = BuildLog(
+        task_id=info["id"]+'-PUBLISH',
+        course_key=course_key,
+        instance_key=instance_key,
+        start_time=now,
+        status=States.PUBLISH,
+        action=Action.CLONE
+    )
+    print('build_log')
+
+    db.session.add(new_log_entry)
+    db.session.commit()
 
 
 @task_postrun.connect
@@ -134,36 +139,6 @@ def clone_task_postrun(task_id=None, sender=None, *args, **kwargs):
         db.session.add(new_log_entry)
         db.session.commit()
 
-
-# @task_success.connect
-# def clone_task_success(task_id=None, sender=None, *args, **kwargs):
-#     # information about task are located in headers for task messages
-#     # using the task protocol version 2.
-#     print('task_success')
-#     now = datetime.utcnow()
-#     logger.info('task_success for task id {}'.format(
-#         task_id
-#     ))
-#     with celery.app.app_context():
-#         old_log_entry = BuildLog.query.filter_by(
-#             task_id=task_id+'-RUNNING',
-#             action=Action.CLONE if sender.__name__ is 'pull_repo' else Action.BUILD
-#         ).first()
-#         old_log_entry.end_time = now
-#         new_log_entry = BuildLog(
-#             task_id=task_id+'-SUCCESS',
-#             course_key=old_log_entry.course_key,
-#             instance_key=old_log_entry.instance_key,
-#             start_time=now,
-#             status=States.SUCCESS,
-#             action=Action.CLONE if sender.__name__ is 'pull_repo' else Action.BUILD,
-#             log_text=kwargs['result']
-#         )
-#         build = Build.query.filter_by(course_key=old_log_entry.course_key, instance_key=old_log_entry.instance_key).first()
-#         build.status = States.SUCCESS
-#         db.session.add(new_log_entry)
-#         db.session.commit()
-#
 
 @task_failure.connect
 def clone_task_failure(task_id=None, sender=None, *args, **kwargs):
