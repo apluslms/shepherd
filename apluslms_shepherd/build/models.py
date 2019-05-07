1f45063f25fee8c9da0bf491c743d936ebe5b15e
Ruiyang Ding <dry950526@gmail.com>
2019-05-07 16:15:47 +0300

    Fix constraint bug

--- apluslms_shepherd/build/models.py
+++ apluslms_shepherd/build/models.py
@@ -16,7 +16,7 @@ class Action(enum.Enum):
 
 
 class Build(db.Model):
-    id = db.Column(db.Integer, autoincrement=True)
+    # id = db.Column(db.Integer, autoincrement=True)
     instance_id = db.Column(db.Integer, db.ForeignKey('course_instance.id'), primary_key=True)
     course_key = db.Column(db.String(50))
     instance_key = db.Column(db.String(50))
@@ -25,16 +25,18 @@ class Build(db.Model):
     end_time = db.Column(db.DateTime)
     state = db.Column(db.Enum(States))
     action = db.Column(db.Enum(Action))
-    log = db.relationship('BuildLog', backref='build', cascade="all,delete")
+    # log = db.relationship('BuildLog', backref='build', cascade="all,delete")
 
 
 class BuildLog(db.Model):
-    id = db.Column(db.Integer, db.ForeignKey('build.id'))
-    instance_id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, db.ForeignKey('build.id'))
+    instance_id = db.Column(db.Integer, db.ForeignKey('build.instance_id'), primary_key=True)
+    number = db.Column(db.Integer, db.ForeignKey('build.number'), primary_key=True)
     course_key = db.Column(db.String(50))
     instance_key = db.Column(db.String(50))
-    number = db.Column(db.Integer, primary_key=True)
     action = db.Column(db.Enum(Action), primary_key=True)
     start_time = db.Column(db.DateTime)
     end_time = db.Column(db.DateTime)
     log_text = db.Column(db.Text)
+    instance = db.relationship("Build", foreign_keys=[instance_id])
+    number_r = db.relationship("Build", foreign_keys=[number])
