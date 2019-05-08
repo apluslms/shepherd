e6d38b657cb05134e8b7460eb72825e8c4a20b2a
Ruiyang Ding <dry950526@gmail.com>
2019-05-08 13:58:52 +0300

    github support

--- apluslms_shepherd/webhooks/view.py
+++ apluslms_shepherd/webhooks/view.py
@@ -10,9 +10,11 @@ from apluslms_shepherd.celery_tasks.tasks import pull_repo, build_repo, error_ha
 
 webhooks_bp = Blueprint('webhooks', __name__, url_prefix='/hooks/')
 
-
-@webhooks_bp.route('pushed/', methods=['POST'])
-def pushed():
+'''
+Only support push at moment
+'''
+@webhooks_bp.route('gitlab/', methods=['POST'])
+def gitlab():
     base_path = config.DevelopmentConfig.COURSE_REPO_BASEPATH
     update_type = request.headers.get('X-Gitlab-Event')
     gitlab_token = request.headers.get('X-GitLab-Token')
@@ -25,11 +27,53 @@ def pushed():
         git_branch = data['ref'].split('/')[2]
         git_ssh_url = data['repository']['git_ssh_url']
         git_http_url = data['repository']['git_http_url']
-        print("Git Push detected: git_ssh_url:{}, git_http_url:{}, on branch {}".format(git_ssh_url, git_http_url,
+        print("Git Push at GitLab detected: git_ssh_url:{}, git_http_url:{}, on branch {}".format(git_ssh_url, git_http_url,
                                                                                         git_branch))
         instance = CourseInstance.query \
             .filter((CourseInstance.git_origin == git_ssh_url) | (CourseInstance.git_origin == git_http_url)) \
             .filter_by(secret_token=gitlab_token, branch=git_branch) \
+            .first()
+        if instance is None:
+            abort(401, "Invalid token or no matching local course instance")
+        if config.Config.USE_SSH_FOR_GIT:
+            use_url = git_ssh_url
+        else:
+            use_url = git_http_url
+
+            # Run task
+        # pull_s = pull_repo.s(base_path, use_url, git_branch, instance.course_key, instance.key)
+        build_s = build_repo.s(base_path, instance.course_key, instance.key)
+        pull_repo.apply_async(args=[base_path, use_url, git_branch, instance.course_key, instance.key],
+                              link=build_s,
+                              link_error=error_handler.s())
+
+
+    else:
+        abort(400, "Invalid payload")
+    return 'hi from a+'
+
+'''
+Only support push at moment
+'''
+@webhooks_bp.route('github/', methods=['POST'])
+def github_pushed():
+    base_path = config.DevelopmentConfig.COURSE_REPO_BASEPATH
+    update_type = request.headers.get('X-GitHub-Event')
+    token = request.headers.get('X-Hub-Signature')
+    data = json.loads(request.data.decode('utf-8'))
+    if not update_type:
+        abort(400, 'No X-GitHub-Event header given')
+    if str(update_type) != "push":
+        abort(400, "Not a push event")
+    if 'ref' and 'repository' in data and 'url' in data['repository']:
+        git_branch = data['ref'].split('/')[2]
+        git_ssh_url = data['repository']['ssh_url']
+        git_http_url = data['repository']['clone_url']
+        print("Git Push at GitLab detected: git_ssh_url:{}, git_http_url:{}, on branch {}".format(git_ssh_url, git_http_url,
+                                                                                        git_branch))
+        instance = CourseInstance.query \
+            .filter((CourseInstance.git_origin == git_ssh_url) | (CourseInstance.git_origin == git_http_url)) \
+            .filter_by(secret_token=token, branch=git_branch) \
             .first()
         if instance is None:
             abort(401, "Invalid token or no matching local course instance")
