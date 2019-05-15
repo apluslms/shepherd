d789f896cf2fdc2f0215199d7938636e5998dbb6
Ruiyang Ding <dry950526@gmail.com>
2019-05-15 13:48:57 +0300

    Using websocket to update frontend display from broker.
    fix display bug

--- apluslms_shepherd/static/socketio.js
+++ apluslms_shepherd/static/socketio.js
@@ -1,10 +1,11 @@
-    // var socket = io.connect(window.location.href);
-    // socket.on('update', function(data) {
-    //     console.log(data)
-    //     $("#"+data.instance_id+".table-number").html(data.build_number);
-    //     $("#"+data.instance_id+".table-action").html(data.current_action);
-    //     $("#"+data.instance_id+".table-state").html(data.current_state);
-    // });
-    //     socket.on('connect', function() {
-    //   console.log('connect')
-    // });
+
+var socket = io.connect('http://' + document.domain + ':' + 5001);
+    socket.on('update', function(data) {
+        console.log(data)
+        $("#"+data.instance_id+".table-number").html(data.build_number);
+        $("#"+data.instance_id+".table-action").html(data.current_action);
+        $("#"+data.instance_id+".table-state").html(data.current_state);
+    });
+        socket.on('connect', function() {
+      console.log('connect')
+    });
