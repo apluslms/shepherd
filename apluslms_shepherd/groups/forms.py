dd12832a9352be9e6ad89582ed62d65c5689995e
QianqianQ <qianqian.qin@outlook.com>
2019-05-15 14:55:36 +0300

    fix the bug that a group with a null name could be added. now the group name must be given

--- apluslms_shepherd/groups/forms.py
+++ apluslms_shepherd/groups/forms.py
@@ -2,7 +2,7 @@ from wtforms import Form, StringField, validators, TextAreaField, BooleanField
 
 
 class GroupForm(Form):
-    name = StringField('Group', [validators.Length(max=50)])
+    name = StringField('Group name', [validators.InputRequired(), validators.Length(max=50) ])
     parent_path = StringField('Parent Path', [validators.Length(max=200)])
 
 
