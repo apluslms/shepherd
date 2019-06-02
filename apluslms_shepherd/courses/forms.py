c816b13434ef942e3719e668488e21a023963873
QianqianQ <qianqian.qin@outlook.com>
2019-06-03 02:35:35 +0300

    link course and group

--- apluslms_shepherd/courses/forms.py
+++ apluslms_shepherd/courses/forms.py
@@ -1,29 +1,21 @@
 from wtforms import Form, StringField, validators, TextAreaField, BooleanField,SelectField
-from wtforms.widgets import html_params
 
 
 class CourseForm(Form):
-    course_key = StringField('Course Key', [validators.InputRequired(), validators.Length(max=50)])
-    instance_key = StringField('Instance Key', [validators.InputRequired(), validators.Length(max=50)])
-    name = StringField('Course Name', [validators.InputRequired(), validators.Length(min=1,max=50)])
-    git_origin = StringField('Git Repo', [validators.InputRequired(), validators.Length(max=255)])
+    key = StringField('Course Key', [validators.Length(max=50)])
+    instance_key = StringField('First Instance Key', [validators.Length(max=50)])
+    name = StringField('Course Name', [validators.Length(max=50)])
+    git_origin = StringField([validators.Length(max=255)])
+    change_all = BooleanField('Change git origin to all instance?')
+    branch = StringField('First Instance Branch', [validators.Length(max=50)])
+    owner = SelectField('Group',coerce=int)
+
+
+class InstanceForm(Form):
+    key = StringField('Instance Key', [validators.Length(max=50)])
+    git_origin = StringField('Git Origin', [validators.Length(max=255)])
     secret_token = StringField('Secret Token', [validators.length(max=127), validators.optional(True)],
-                               render_kw={
-                                   "placeholder": "If no secret token set for GitHub/Gitlab webhook, leave this empty."
-                               })
-
-    branch = StringField('Branch', [validators.InputRequired(),validators.Length(max=50)])
+                               render_kw={"placeholder": "If no secret token set for GitHub/Gitlab webhook, leave this empty."})
+    branch = StringField('Instance Branch', [validators.Length(max=50)])
     config_filename = StringField('Roman Config File', [validators.length(max=127), validators.optional(True)],
-                                  render_kw={"placeholder": "To use default filename(course.yml), leave this empty."})
-    # The options of the identity group which has the permission to create new courses 
-    # and the current user is the member of
-    identity = SelectField('Identity', choices=[(-1, "---")], coerce=int)
-    # The options of the group that owns the course
-    owner_group = SelectField('Owner Group', choices=[(-1, "---")], coerce=int)
-
-
-class CourseEditForm(Form):
-    key = StringField('Course Key', [validators.InputRequired(), validators.Length(max=50)])
-    name = StringField('Course Name', [validators.InputRequired(), validators.Length(min=1,max=50)])
-    git_origin = StringField([validators.Optional(), validators.Length(max=255)])
-    identity = SelectField('Identity',choices=[(-1, "---")], coerce=int)
+                                  render_kw={"placeholder" : "To use default filename(course.yml), leave this empty."})
--- apluslms_shepherd/courses/models.py
+++ apluslms_shepherd/courses/models.py
@@ -1,16 +1,20 @@
 from apluslms_shepherd.extensions import db
 from apluslms_shepherd.config import DevelopmentConfig
-from apluslms_shepherd.groups.models import gc_table
+
+class CourseRepository(db.Model):
+    key = db.Column(db.String(50), unique=True, primary_key=True)
+    name = db.Column(db.String(50), default='')
+    owner_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    owner = db.relationship("Group", backref=db.backref("courses", cascade="all,delete"))
+    instance = db.relationship('CourseInstance', backref='course_repository', cascade="all,delete")
 
 
 class CourseInstance(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    course_key = db.Column(db.String(50), nullable=False)
-    name = db.Column(db.String(50), default='')
-    owners = db.relationship("Group", secondary=gc_table, backref=db.backref('courses', lazy='dynamic'))
-    instance_key = db.Column(db.String(50), nullable=False)
+    key = db.Column(db.String(50), nullable=False)
+    course_key = db.Column(db.String(50), db.ForeignKey('course_repository.key'), nullable=False)
     git_origin = db.Column(db.String(255), default='')
     secret_token = db.Column(db.String(127))
     config_filename = db.Column(db.String(127))
     branch = db.Column(db.String, default='master')
-    builds = db.relationship('Build', lazy='select', backref=db.backref('course', lazy='joined'))
+    builds = db.relationship('Build', lazy='select', backref=db.backref('instance', lazy='joined'))
