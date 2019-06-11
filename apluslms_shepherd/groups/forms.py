d72b53090c47ebf6fa174f84d17c7f2465e1bcee
QianqianQ <qianqian.qin@outlook.com>
2019-06-11 10:22:47 +0300

    add comments and remove redudant

--- apluslms_shepherd/groups/forms.py
+++ apluslms_shepherd/groups/forms.py
@@ -1,38 +1,21 @@
-from wtforms import Form, StringField, validators, SelectMultipleField, \
-                    FormField,FieldList,SelectField
-from wtforms.widgets import ListWidget, CheckboxInput,html_params
+from wtforms import Form, StringField, validators, SelectMultipleField
+from wtforms.widgets import html_params, ListWidget, CheckboxInput 
 
-from apluslms_shepherd.groups.models import PERMISSION_LIST
+from apluslms_shepherd.groups.utils import PERMISSION_LIST
 
-def select_multi_checkbox(field, ul_class='', **kwargs):
+# Widget that renders a SelectMultipleField as a collection of checkboxes 
+def select_multi_checkbox(field, input_class='', **kwargs):
     kwargs.setdefault('type', 'checkbox')
     field_id = kwargs.pop('id', field.id)
-    html = [u'<div %s>' % html_params(id=field_id, class_=ul_class)]
+    html = [u'<div %s>' % html_params(id=field_id, class_=input_class)]
     for value, label, checked in field.iter_choices():
         choice_id = u'%s-%s' % (field_id, value)
         options = dict(kwargs, name=field.name, value=value, id=choice_id)
         if checked:
             options['checked'] = 'checked'
-        # html.append(u'<li><input %s /> ' % html_params(**options))
         html.append(u'&nbsp; <input %s /> ' % html_params(**options))
-        # html.append(u'<label for="%s">%s</label></li>' % (field_id, label))
         html.append(u'<label for="%s">%s</label> &nbsp;' % (field_id, label))
-    # html.append(u'</ul>')
     html.append(u'</div>')    
-    return u''.join(html)
-
-def select_multi_checkbox2(field, ul_class='', **kwargs):
-    kwargs.setdefault('type', 'checkbox')
-    field_id = kwargs.pop('id', field.id)
-    html = [u'<ul %s>' % html_params(id=field_id, class_=ul_class)]
-    for value, label, checked in field.iter_choices():
-        choice_id = u'%s-%s' % (field_id, value)
-        options = dict(kwargs, name=field.name, value=value, id=choice_id)
-        if checked:
-            options['checked'] = 'checked'
-        html.append(u'<li><input %s /> ' % html_params(**options))
-        html.append(u'<label for="%s">%s</label></li>' % (field_id, label))
-    html.append(u'</ul>')
     return u''.join(html)
 
 
@@ -46,6 +29,7 @@ class GroupForm(Form):
     parent_path = StringField('Parent Path', [validators.Length(max=200)])
     permissions = SelectMultipleField('Permisson', choices=PERMISSION_LIST,widget=select_multi_checkbox)
     course_prefix = StringField('Course Prefix', [validators.Optional()])
-    target_groups = SelectMultipleField('Parents of Subgroups',[validators.Optional()],choices=[(0,'Itself')],coerce=int,widget=select_multi_checkbox)
+    target_groups = SelectMultipleField('Parents of Subgroups',[validators.Optional()],choices=[(0,'Itself')],
+                                    coerce=int,widget=select_multi_checkbox)
 
     
--- apluslms_shepherd/groups/models.py
+++ apluslms_shepherd/groups/models.py
@@ -1,30 +1,27 @@
+from apluslms_shepherd.extensions import db
+from sqlalchemy_mptt.mixins import BaseNestedSets
 import enum
 import re
-from sqlalchemy_mptt.mixins import BaseNestedSets
-from apluslms_shepherd.extensions import db
-# db.metadata.clear()
 
+# Association tables for ManyToMany relationships
+# For Group model and User model
 gm_table = db.Table('gm_table', db.Model.metadata,
                     db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
                     db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
                     )
-
+# For Group model and Permission model
 gp_table = db.Table('gp_table', db.Model.metadata,
                     db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
                     db.Column('permission_id', db.Integer, db.ForeignKey('group_permission.id'))
                     )
-
+# For Group model and CourseRepository model 
 gc_table = db.Table('gc_table', db.Model.metadata,
                     db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
                     db.Column('course_key', db.String, db.ForeignKey('course_repository.key'))
                     )
 
-# admin_table = db.Table('admin_table', db.Model.metadata,
-#                     db.Column('group_id', db.Integer, db.ForeignKey('group.id')),
-#                     db.Column('admin_group_id', db.Integer, db.ForeignKey('group.id'))
-#                     )
 
-
+# CRUD class
 class CRUD():
     def save(self):
         db.session.add(self)
@@ -37,16 +34,13 @@ class CRUD():
 
 class Group(db.Model, BaseNestedSets, CRUD):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), index=True, nullable=False)
+    name = db.Column(db.String(50), index=True, nullable=False)
     members = db.relationship("User", secondary=gm_table,
                               backref=db.backref('groups', lazy='dynamic'))
     permissions = db.relationship("GroupPermission", secondary=gp_table,
                                   backref=db.backref('groups', lazy='dynamic'))
     self_admin = db.Column(db.Boolean,default=True)
-    # admins = db.relationship("Group", secondary=admin_table, 
-    #                        primaryjoin=id==admin_table.c.group_id,
-    #                        secondaryjoin=id==admin_table.c.admin_group_id,
-    #                     )
+
     def __init__(self, name, parent_id=None):
         self.name = name
         self.parent_id = parent_id
@@ -58,36 +52,10 @@ class Group(db.Model, BaseNestedSets, CRUD):
             return "<Group (id={0}, name={1}, parent={2})>".format(self.id, self.name,
                                                                    self.parent.name)
 
-    # def is_admin(self, user):   
-
-    #     # 1. there is an admin permission and the user is member of the permission's group 
-    #     #    and the target_group_id is this group
-    #     if self.self_admin:
-    #         return True
-    #     # OR:
-    #     # 2. user is member of ancestors
-    #     ancestors = self.path_to_root().all()
-    #     for group in ancestors:
-    #         if user in group.members:
-    #             return True
-
-
-# Group and AdminOfGroup: Many To Many 
-# class GroupAdmin(db.Model):
-#     group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
-#     # admin_id = db.Column(db.Integer, db.ForeignKey('group.id'))
-#     groups = db.relationship("Group", secondary=admin_table,
-#                                   backref=db.backref('admins', lazy='dynamic'))
-
-
-PERM_TYPE = {'self_admin':'self-administrator',
-            'groups': 'create subgroups','courses': 'create courses'}
-PERMISSION_LIST = list(perm_tuple for perm_tuple in PERM_TYPE.items())
-
 
 class PermType(enum.Enum):
     self_admin = 1
-    groups = 2
+    subgroups = 2
     courses = 3
     
 
@@ -97,25 +65,28 @@ class GroupPermission(db.Model):
 
 
 class CreateGroupPerm(db.Model):
-    # id = db.Column(db.Integer,primary_key=True)
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'),primary_key=True)
     target_group_id = db.Column(db.Integer, db.ForeignKey('group.id'),primary_key=True)
     group = db.relationship('Group',foreign_keys=[group_id],backref=db.backref("group_perm", 
                             uselist=False,cascade='all,delete'))
-    target_group = db.relationship('Group',foreign_keys=[target_group_id],backref=db.backref("group_perm_as_target", 
-                            uselist=False,cascade='all,delete'))
+    target_group = db.relationship('Group',foreign_keys=[target_group_id],
+                                backref=db.backref("group_perm_as_target", 
+                                uselist=False,cascade='all,delete'))
+
 
 class CreateCoursePerm(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
-    group = db.relationship("Group", backref=db.backref("course_permission", uselist=False,cascade='all,delete'))
+    group = db.relationship("Group", backref=db.backref("course_permission", 
+                            uselist=False,cascade='all,delete'))
     regexp = db.Column(db.Boolean,default=True)
     pattern = db.Column(db.String(30))
 
     def pattern_match(self,course_name):
-        # None or "cs-*" or "^cs-[a-c][0-9]+$" # fnmatch.fnmatch vs. re.match
-        if self.pattern is None:
-            return True
+        """Check whether the course_name meet the pattern requirement.
+        """
+        if self.pattern is None:  # No pattern requirement needed
+            return True 
         else:
             flag = re.match(self.pattern,course_name)
             if flag:
