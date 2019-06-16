92264095ddd797d8381b80bbf80ef809bb5c8746
QianqianQ <qianqian.qin@outlook.com>
2019-06-16 15:34:15 +0300

    fix the bug in moving courses. Change the course list ui

--- apluslms_shepherd/static/my_groups.js
+++ apluslms_shepherd/static/my_groups.js
@@ -1,17 +1,19 @@
+var old_owner_id = null;
+
 $(function() { // Delete a group and remove it from the group list
 
     // Trigger the delete event
     $('.delete').click(function(event) {
         var group_id = $(this).val();
+        old_owner_id = $(this).val();  // Change the value of old_owner_id for removing courses
         $.ajax({
         // Add the group_id query string to the url
         url: '/groups/delete/'+'?group_id='+group_id,  
         type: 'POST',
         success:function(data){  // The group is deleted successfully
-
-        alert('The group has been deleted');
         // Remove the group element
         var group = $('.group[value='+group_id+']');
+        alert('The group has been deleted');
         group.remove();
         },
         statusCode: {
@@ -19,7 +21,6 @@ $(function() { // Delete a group and remove it from the group list
                 error = JSON.parse(response.responseText)
                 alert(error.message);
                 $('#moveCourseModal').modal();
-                move_courses(group_id);
             },
             501:function (response) {
                 error = JSON.parse(response.responseText)
@@ -32,9 +33,9 @@ $(function() { // Delete a group and remove it from the group list
 });      
 
 
-$(function() {  // Update the dropdown options of new owner group
+$(function move_courses() {  
 
-    fetch('/groups/options_of_new_owner/')  // fetch the groups from the database
+    fetch('/groups/options_of_new_owner/')  // Update the dropdown options of new owner group
     .then(function(response){
         if (response.status !== 200) {  
             console.log('Error occurs. Status Code: ' +
@@ -48,14 +49,8 @@ $(function() {  // Update the dropdown options of new owner group
             }
         })
     })
-});
 
-
-function move_courses(old_owner_id) {  // Post the request of moving courses
-
-    var test='/groups/move_course/'+'?old_owner_id='+old_owner_id+'&new_owner_id='+$('#new_owner').val();  
-    console.log(test)
-    $('#move_courses').click(function(event) {
+    $('#move_courses').click(function(event) { // Post the request of moving courses from the old owner
         $.ajax({
         // Add the group_id query string to the url
         url: '/groups/move_course/'+'?old_owner_id='+old_owner_id+'&new_owner_id='+$('#new_owner').val(),  
@@ -71,4 +66,4 @@ function move_courses(old_owner_id) {  // Post the request of moving courses
         // Stop the browser from submitting the form.
         event.preventDefault();
     });   
-};      
+});      
