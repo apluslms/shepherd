f4fa6025b8b8f598de5dccd90399a16e11ed4ce3
Ruiyang Ding <dry950526@gmail.com>
2019-08-21 17:43:32 +0300

    Fix 0cdd8a8d39687e89ef2123639b4c923653e856f5: Cache all log in one step and dump them to database in the end of each step.

--- apluslms_shepherd/observer/observer.py
+++ apluslms_shepherd/observer/observer.py
@@ -16,6 +16,7 @@ class ShepherdObserver(BuildObserver):
         self.logger = logger
         self.broker = broker
         self.starts = {}
+        self.log_cache = {}
         self.logs = {}
 
     def set_logger(self, logger: Callable):
@@ -30,15 +31,16 @@ class ShepherdObserver(BuildObserver):
         if type_ == ShepherdMessage.END:
             end = datetime.utcnow()
             start = self.starts.get(step, 0)
+            # Send all left message to frontend logger
+            self.broker.notify(self.log_cache[step])
+            # Append these sent log to logs, which has all previous logs.
+            self.logs.setdefault(step, []).extend(self.log_cache[step])
             # The log of each step should be like {'step1':[{'state': 'state for log 1', 'step': 'step for log1',
             # 'log': 'log 1 content' }, {same dict for log2}, ...]}
             logs = self.logs.get(step, [])
-            # Write this log entry to db
+            # Write all log in this step log entry to db
             self.logger(step, start, end, state, '\n'.join([l['log'] for l in logs]))
-            # Send all left message to frontend logger
-            self.broker.notify(self.logs[step])
-            # Empty log cache after send log to frontend.
-            self.logs[step] = []
+            self.log_cache[step] = []
         if type_ == ShepherdMessage.UPDATE:
             # Only update frontend, no log.
             frontend_msg = [{'step': step.name, 'state': state.name, 'roman_step': None, 'log': ''}]
@@ -48,23 +50,27 @@ class ShepherdObserver(BuildObserver):
             # Composing a log entry
             log_txt = fmt.format(phase.name, str(data).rstrip())
             frontend_msg = {'step': step.name, 'state': state.name, 'roman_step': None, 'log': log_txt}
-            self.logs.setdefault(step, []).append(frontend_msg)
+            self.log_cache.setdefault(step, []).append(frontend_msg)
             # We store the course info in logger function
             # Notify broker every 10 lines of log
-            if len(self.logs[step]) > 10:
-                self.broker.notify(self.logs[step])
-                self.logs[step] = []
+            if len(self.log_cache[step]) > 10:
+                self.broker.notify(self.log_cache[step])
+                # Append log in cache to logs, which has all previous logs for every step.
+                self.logs.setdefault(step, []).extend(self.log_cache[step])
+                # Empty cache
+                self.log_cache[step] = []
         if type_ == Message.CONTAINER_MSG:
             fmt = '{} >> {}'
             log_txt = fmt.format(phase.name, str(data).rstrip())
             celery_logger.info(log_txt)
             frontend_msg = {'step': BuildStep.BUILD.name, 'state': BuildState.RUNNING.name, 'roman_step': str(step),
                             'log': log_txt}
-            self.logs.setdefault(BuildStep.BUILD, []).append(frontend_msg)
+            self.log_cache.setdefault(BuildStep.BUILD, []).append(frontend_msg)
             # Notify broker every 10 lines of log
-            if len(self.logs[BuildStep.BUILD]) > 10:
-                self.broker.notify(self.logs[BuildStep.BUILD])
-                self.logs[BuildStep.BUILD] = []
+            if len(self.log_cache[BuildStep.BUILD]) > 10:
+                self.broker.notify(self.log_cache[BuildStep.BUILD])
+                self.logs.setdefault(BuildStep.BUILD, []).extend(self.log_cache[BuildStep.BUILD])
+                self.log_cache[BuildStep.BUILD] = []
 
     def shepherd_step_start(self, step):
         self._message(self._phase, ShepherdMessage.START, step)
